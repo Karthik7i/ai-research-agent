850931d8c561d1c359349fd5bbe26bf28de82807
Karthik Cheekuri <karthikeyansanju2@gmail.com>
2026-08-11 23:27:53 +0530

    Connect research agent to prediction tool

--- src/research_agent.py
+++ src/research_agent.py
@@ -1,12 +1,12 @@
-from src.predictor import Predictor
+from src.tools.prediction_tool import PredictionTool
 
 
 class ResearchAgent:
-    def __init__(self, predictor: Predictor):
-        self.predictor = predictor
+    def __init__(self, prediction_tool: PredictionTool):
+        self.prediction_tool = prediction_tool
 
     def research(self, symbol: str) -> dict:
-        prediction = self.predictor.predict_symbol(symbol)
+        prediction = self.prediction_tool.run(symbol)
 
         return {
             "symbol": symbol,
